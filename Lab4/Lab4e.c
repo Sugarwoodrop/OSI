8f8361fd7f24a86f3d1f7a973a66f77a84a3d55f
Sugarwoodrop <135331725+Sugarwoodrop@users.noreply.github.com>
2025-05-05 08:34:15 +0700

    Create Lab4e.c

---
+++ Lab4/Lab4e.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+void part_e()
+{
+    char *buf1 = malloc(100);
+    strcpy(buf1, "hello world");
+    printf("buf1 before free : %s\n", buf1);
+    free(buf1);
+    printf("buf1 after free  : %s\n", buf1);
+
+    char *buf2 = malloc(100);
+    strcpy(buf2, "hello again");
+    printf("buf2 new alloc   : %s\n", buf2);
+
+    char *mid = buf2 + 50;
+    free(mid);
+    printf("buf2 after bad free: %s\n", buf2);
+
+    free(buf2);
+}
+
+int main(void)
+{
+    part_e();
+
+    return 0;
+}

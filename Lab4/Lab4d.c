82a49904ded8ecba1010230db642ef4d4efcae83
Sugarwoodrop <135331725+Sugarwoodrop@users.noreply.github.com>
2025-05-19 08:07:11 +0700

    Update Lab4d.c

--- Lab4/Lab4d.c
+++ Lab4/Lab4d.c
@@ -4,14 +4,14 @@ int *bad_return()
 {
     int local = 123;
     int *local_p = &local;
-    printf("-- Part d -- returned %p, value %d (неопределено)\n", (void *)local_p, *local_p);
+    printf("-- Part d -- returned %p, value %d\n", (void *)local_p, *local_p);
     return local_p;
 }
 
 void part_d()
 {
     int *local_address = bad_return();
-    printf("-- Part d -- returned %p, value %d (неопределено)\n", (void *)local_address, *local_address);
+    printf("-- Part d -- returned %p, value %d\n", (void *)local_address, *local_address);
 }
 
 int main(void)
